5d384d7c293be3fa5b35d0e6fc65da8073c1615e
Your Name <81007262+TheMulhima@users.noreply.github.com>
2021-10-27 16:36:36 +0400

    Initial commit

---
+++ SpeedRunQoL/KeyBinds.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using DebugMod;
+using UnityEngine;
+
+namespace SpeedRunQoL
+{
+    //needs to be a public static class
+    public static class KeyBinds
+    {
+        //each keybind that has to be loaded needs to be annotated with BindableMethod and be public static void
+        [BindableMethod(name = "Reload Radiance Fight", category = "Speedrun Extentions")]
+        public static void LoadRadiance()
+        {
+            GameManager.instance.StartCoroutine(LoadRadianceRoom());
+            
+            //making sure debugmod console is logged when something happens is good
+            DebugMod.Console.AddLine("Radiance Loaded");
+        }
+        [BindableMethod(name = "Press Accept Challenge", category = "Speedrun Extentions")]
+        public static void ChallengeRadiance()
+        {
+            GameObject.Find("Challenge Prompt Radiant").LocateMyFSM("Challenge Start").SetState("Take Control");
+            DebugMod.Console.AddLine("Radiance Challenged (asuming it exists))");
+        }
+        
+        //since this function is private static and isnt annotated with BindableMethod, it wont show up as keybind
+        private static IEnumerator LoadRadianceRoom()
+        {
+            //makes sure the initial platform and challage prompt appears
+            HeroController.instance.gameObject.LocateMyFSM("ProxyFSM").FsmVariables.FindFsmBool("Faced Radiance").Value = false;
+            
+            HeroController.instance.RelinquishControl();
+            HeroController.instance.StopAnimationControl();
+            EventRegister.SendEvent("START DREAM ENTRY");
+            EventRegister.SendEvent("DREAM ENTER");
+            HeroController.instance.enterWithoutInput = true; // stop early control on scene load
+            
+            GameManager.instance.BeginSceneTransition(new GameManager.SceneLoadInfo
+            {
+                SceneName = "Dream_Final_Boss",
+                EntryGateName = "door1",
+                EntryDelay = 0f,
+                PreventCameraFadeOut = true,
+            });
+
+            yield return new WaitUntil(() => HeroController.instance.acceptingInput);
+            yield return null;
+            //cuz people mostly have full soul from thk fight
+            HeroController.instance.AddMPCharge(198);  
+        }
+    }
+}
---
+++ SpeedRunQoL/SpeedRunQoL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using DebugMod;
+using Modding;
+using MonoMod;
+using UnityEngine;
+
+namespace SpeedRunQoL
+{
+    //debug doesnt do any of the dll loading so the mod needs to inherit from "Mod" so the modding api loads it
+    public class SpeedRunQoL: Mod
+    {
+        public override string GetVersion() => "v0.1";
+        
+        public override void Initialize()
+        {
+            //checks whether debug is loaded. probably could also use
+            //"ModHooks.GetMod("DebugMod") is Mod" or
+            //"ModHooks.LoadedModsWithVersions.ContainsKey("DebugMod")
+            if (AppDomain.CurrentDomain.GetAssemblies().Select(x => x.GetName().Name).Contains("DebugMod"))
+            {
+                AddStuffToDebug();
+            };
+        }
+
+        //making this a different function is the way that the mod doesnt fail to load when debug isnt downloaded.
+        //not so relevant here but doesnt hurt to include it
+        private void AddStuffToDebug()
+        {
+            //The function that needs to be called to add keybinds
+            //the function exists in DebugMod.cs if you wanna see what it does
+            // the parameter is takes the the Typeof(ClassMade)
+            DebugMod.DebugMod.AddToKeyBindList(typeof(KeyBinds));
+        }
+    }
+}
